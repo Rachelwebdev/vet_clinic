54e125886d6fb78d8c95bb55a637eb69c388f1ff
Rachel Isaac <rachelisaac995@gmail.com>
2023-03-27 19:29:59 +0400

    Add SQL to schema file

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE animals(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL
+);
